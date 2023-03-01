050e9d4c86dbe39a19224d4d6d49a67fcbeb2b64
Athira M Chandran <86779388+Athira-M-Chandran@users.noreply.github.com>
2023-03-01 13:31:50 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+//prerequistes
+
+import React, { Component }from "react";
+
+
+// extends the component class react
+class App extends Component{
+    
+    render(){
+        return(
+            <div>
+                React App
+            </div>
+        )
+    }
+}
+
+export default App;
+
+
+
